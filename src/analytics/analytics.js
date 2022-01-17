60067ae628dda717a9ca26eb85719a7c6ac2b341
bradley vangelder <r0784535@student.thomasmore.be>
2022-01-17 14:01:27 +0100

    analytics

---
+++ src/analytics/analytics.js
@@ -0,0 +1,22 @@
+const me = document.querySelector('script[lancero-pk]');
+const pk = me.getAttribute('lancero-pk');
+const referrer = document.referrer;
+
+
+document.onreadystatechange = async () => {
+    if (document.readyState === 'complete') {
+        const userInfo = await fetch('http://ip-api.com/json').then(res => res.json())
+
+        if(!referrer.includes('localhost') && !referrer.includes('127.0.0.1')) {
+            await fetch('https://api.lancero.app/projects/addvisit', {
+                method: 'POST',
+                body: JSON.stringify({
+                    referredSite: referrer,
+                    ip: userInfo.query,
+                    country: userInfo.country,
+                }),
+                headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${pk}`},
+            })
+        }
+    }
+};
